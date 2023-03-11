7fd7a8f87b5caed6e2edceb18bf340f3cfdca155
PawelDuplaga <pawelduplagait@gmail.com>
2023-03-11 11:54:11 +0100

    constructor and small functions

--- NeuralNetwork.cs
+++ NeuralNetwork.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MatrixAlgebraForDoubles;
+using System.Runtime.CompilerServices;
+
+public class NeuralNetwork
+{
+    private int numberOfHiddenLayers { get; set; }
+    public List<Matrix> LAYERS  = new List<Matrix>();
+    public List<Matrix> WEIGHTS = new List<Matrix>();
+
+    public NeuralNetwork(int numberOfHiddenLayers, int defaultSizeOfLayers)
+    {
+        this.numberOfHiddenLayers = numberOfHiddenLayers;
+        for(int i =0; i< numberOfHiddenLayers; i++)
+        {
+            LAYERS.Add(new Matrix(1,defaultSizeOfLayers));
+
+        }
+    }
+
+
+    public Matrix GetInputs() => LAYERS.First();
+    public Matrix GetOutput() => LAYERS.Last(); 
+    
+
+}
