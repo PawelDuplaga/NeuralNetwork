f09cd95cabea62da2f6f82899821d1f988755984
PawelDuplaga <pawelduplagait@gmail.com>
2023-03-11 19:41:35 +0100

    Working on clean code

--- NeuralNetwork.cs
+++ NeuralNetwork.cs
@@ -1,20 +1,24 @@
 ﻿using MatrixAlgebraForDoubles;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 public class NeuralNetwork
 {
     private int numberOfHiddenLayers { get; set; }
     public List<Matrix> LAYERS = new List<Matrix>();
     public List<Matrix> WEIGHTS = new List<Matrix>();
+    public List<Func<double,double>> CUSTOM_ACTIVATION_FUNCTIONS = new List<Func<double,double>>();
 
-    enum ActivationFunction
+
+    public enum ActivationFunction
     {
-        ReLU
+        ReLU ,
+        Sigmoid,
+        Custom,
+        None
     }
 
-    private Dictionary<ActivationFunction, double> ActivateFunctions =
-        new Dictionary<ActivationFunction, double>() { };
- 
+
 
 
     public NeuralNetwork(int numberOfHiddenLayers, int defaultSizeOfLayers)
@@ -26,38 +30,76 @@ public class NeuralNetwork
             WEIGHTS.Add(new Matrix(defaultSizeOfLayers,defaultSizeOfLayers));
         }
         LAYERS.Add(new Matrix(1, defaultSizeOfLayers));
+
+        for(int i = 0; i < LAYERS.Count; i++)
+        {
+            //default NO FUNCTION
+            CUSTOM_ACTIVATION_FUNCTIONS.Add(x => x);
+        }
     }
+
+
+    /// <summary>
+    /// Constructor for neural netork using additional parameter - default Function
+    /// <para> Default function is setted like this :</para>
+    /// <br>InputLayer   func = x => x </br>
+    /// <br>HiddenLayer1 func = defaultFunction</br>
+    /// <br>HiddenLayer2 func = defaultFunction</br>
+    /// <br>...</br>
+    /// <br>HiddenLayerN func = defaultFunction</br>
+    /// <br>OutputLayer  func = defaultFunction</br>
+    /// </summary>
+    /// <param name="numberOfHiddenLayers">lol</param>
+    /// <param name="defaultSizeOfLayers"></param>
+    /// <param name="defaultFunction"></param>
+    public NeuralNetwork(int numberOfHiddenLayers, int defaultSizeOfLayers, ActivationFunction defaultFunction)
+    {
+        this.numberOfHiddenLayers = numberOfHiddenLayers;
+        for (int i = 0; i < numberOfHiddenLayers + 1; i++)
+        {
+            LAYERS.Add(new Matrix(1, defaultSizeOfLayers));
+            WEIGHTS.Add(new Matrix(defaultSizeOfLayers, defaultSizeOfLayers));
+        }
+        LAYERS.Add(new Matrix(1, defaultSizeOfLayers));
+
+        for(int i = 0; i < LAYERS.Count; i++)
+        {
+            //default NO FUNCTION
+            CUSTOM_ACTIVATION_FUNCTIONS.Add(x => x);
+        }
+    }
+
 
     public Matrix GetInputs() => LAYERS.First();
     public Matrix GetOutput() => LAYERS.Last();
 
-    public delegate double ActivateFunc(double x);
 
-    public static double ActivateFunctionTransform(double x, ActivateFunc acfunc)
+
+
+
+
+    private Func<double,double> RunActivationFunction(double x, ActivationFunction enum_) 
     {
-        return acfunc(x);
+        switch (enum_) 
+        {
+            case ActivationFunction.ReLU    : return Helpers.ReLU();
+            case ActivationFunction.Sigmoid : return Helpers.Sigmoid();
+            default                         : return Helpers.Sigmoid();
+        }
+
     }
 
-    
-
-
-
-
-
-    //public Matrix RunNetwork(double[] inputs)
-    //{
-    //    if (inputs.Length != GetInputs().columns)
-    //        throw new ArgumentException("Wrong number of inputs given");    
-
-
-    //}
-
-
+    private double RunCustomActivationFunction(double x, Func<double,double> func)
+    {
+        return func(x);
+    }
 
     public NeuralNetwork RandomizeWeigghts()
     {
         for(int i =0; i < this.WEIGHTS.Count; i++)
+        {
             this.WEIGHTS[i].FillRandomInRange(0, 1);
+        }
     
         return this;
     }
@@ -67,9 +109,9 @@ public class NeuralNetwork
         for(int i = 0; i < WEIGHTS.Count; i++)
         {
             LAYERS[i].PrintMatrix();
-            Console.Write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.Write("       ");
             WEIGHTS[i].PrintMatrix();
-            Console.Write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.Write("       ");
         }
         LAYERS[LAYERS.Count - 1].PrintMatrix();
 
@@ -79,10 +121,9 @@ public class NeuralNetwork
     private class Helpers
     {
 
-        public static double ReLU(double x)
-        {
-            return Math.Max(x, 0);
-        }
+        public static Func<double,double> ReLU() => x => Math.Max(x,0);
+        public static Func<double,double> Sigmoid() => x => x;
+
 
 
 
